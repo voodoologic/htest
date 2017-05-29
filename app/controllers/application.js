ca2503ef250e182e2e85ea6847f14603c5955f3b
voodoologic <headley.douglas@gmail.com>
2017-05-28 17:18:04 -0700

    incrementally good.

---
+++ app/controllers/application.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  "actions":{
+    "authenticateWithTwitter": function(){
+    },
+    "logOut": function(){
+    },
+    "ping": function(){
+    }
+  }
+    
+});
